eac668edb85e58ca42ed578d374ca5be05605d72
Karollo <karol.kluba89@gmail.com>
2022-03-05 18:09:38 +0100

    Create databse connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ 
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.HOST,
+      port: +process.env.PORT,
+      username: process.env.USERNAME,
+      password: process.env.PASSWORD,
+      database: process.env.DATABASE,
+      entities: [],
+      synchronize: true
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
