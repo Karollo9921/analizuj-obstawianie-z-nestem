954df1a2f6be4142455e986a22e5cf07c68d9760
Karollo <karol.kluba89@gmail.com>
2022-03-12 21:38:32 +0100

    Defining payload and signing access_token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,19 +5,22 @@ import { UserRepository } from './user.repository';
 import { InjectRepository } from "@nestjs/typeorm";
 import { User } from "./user.entity";
 import * as bcrypt from 'bcrypt';
+import { JwtService } from "@nestjs/jwt";
+import { JwtPayload } from "./jwt-payload.interface";
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserRepository)
-    private userRepository: UserRepository
+    private userRepository: UserRepository,
+    private jwtService: JwtService
   ) {};
 
   async register(registerCredentials: RegisterDto): Promise<void> {
     return await this.userRepository.createUser(registerCredentials);
   };
 
-  async login(loginCredentials: LoginDto): Promise<User> {
+  async login(loginCredentials: LoginDto): Promise<{ access_token: string }> {
     const { loginEmail, password } = loginCredentials;
     const user: User = await User.findOne(
       {
@@ -29,7 +32,10 @@ export class AuthService {
     );
 
     if (user && (await bcrypt.compare(password, user.password))) {
-      return user;
+      const payload: JwtPayload = { login: user.login, email: user.email };
+      const access_token: string = this.jwtService.sign(payload);
+
+      return { access_token };
     } else {
       throw new Error('Something is wrong with your logging credentials...')
     };
