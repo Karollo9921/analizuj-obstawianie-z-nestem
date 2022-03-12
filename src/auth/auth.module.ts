aecddeeb442bee1bf783d7fdaeae9eeac2e50ca3
Karollo <karol.kluba89@gmail.com>
2022-03-12 21:54:20 +0100

    Adding JWT Register Async in Auth Module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,7 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { JwtModule } from "@nestjs/jwt";
+import { PassportModule } from "@nestjs/passport";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
@@ -6,6 +9,16 @@ import { UserRepository } from "./user.repository";
 
 @Module({
   imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          secret: configService.get('JWT_SECRET_ACCESS_TOKEN')
+        };
+      }
+    }),
     TypeOrmModule.forFeature([UserRepository]),
   ],
   controllers: [ AuthController ],
