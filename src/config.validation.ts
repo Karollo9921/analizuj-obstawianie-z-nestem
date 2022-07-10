9ddbbbaac55f4d97179f1a3d5067053ec117bd98
Karollo <karollo9921@gmail.com>
2022-07-10 19:46:08 +0200

    feat: dockerize app

--- src/config.validation.ts
+++ src/config.validation.ts
@@ -1,9 +1,9 @@
 import * as Joi from '@hapi/joi';
 
 export const configValidation = Joi.object({
-  DB_HOST: Joi.string().default('localhost').required(),
-  DB_PORT: Joi.number().default(5432).required(),
-  DB_USERNAME: Joi.string().required(),
-  DB_PASSWORD: Joi.string().required(),
-  DB_DATABASE: Joi.string().required(),
+  POSTGRES_HOST: Joi.string().required(),
+  POSTGRES_PORT: Joi.number().default(5432).required(),
+  POSTGRES_USER: Joi.string().required(),
+  POSTGRES_PASSWORD: Joi.string().required(),
+  POSTGRES_DB: Joi.string().required(),
 });
